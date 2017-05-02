c0272d26233487ba76ab53c097d2dcbffdeb4148
pavic989 <pavicmilos989@gmail.com>
2017-05-02 17:41:09 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
-$('#hamburger_menu').on('click', function(){
-	$('nav').slideToggle('slow');
-})
-
 $(document).ready(function(){
-	var displaySize = $(window).width();
-	var projectMenagerParent = $('.jobsSectionNoBg');
-	var projectMenagerChild = $('.jobsSectionNoBg').find('button.tabletHoverProjectMenager');
+	$('#hamburger_menu').on('click', function(){
+		$('nav').slideToggle('slow');
+	});
+	$(document).ready(function(){
+		var displaySize = $(window).width();
+		var projectMenagerParent = $('.jobsSectionNoBg');
+		var projectMenagerChild = $('.jobsSectionNoBg').find('button.tabletHoverProjectMenager');
 
-	if( displaySize > 320){
-		$('.jobsSectionWithBg').hover(function(){
-			$(this).find('.posloviHover').fadeToggle('slow');
-		});
-		projectMenagerParent.hover(function(){
-			projectMenagerChild.fadeToggle('slow');
-		})
-	} else {
-		return false;
-	};
+		if( displaySize > 320){
+			$('.jobsSectionWithBg').hover(function(){
+				$(this).find('.posloviHover').fadeToggle('slow');
+			});
+			projectMenagerParent.hover(function(){
+				projectMenagerChild.fadeToggle('slow');
+			})
+		} else {
+			return false;
+		};
+	});
+	$('a[data-scroll]').scroller();
 })
 
 
-$('a[data-scroll]').scroller();
+
+
+
