0fc80d05e7254d7b5e042eb46ae20a1d036fd0b1
Miloš Pavić <pavicmilos989@gmail.com>
2017-05-02 03:26:34 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$('#hamburger_menu').on('click', function(){
+	$('nav').slideToggle('slow');
+})
+
+$(document).ready(function(){
+	var displaySize = $(window).width();
+	var projectMenagerParent = $('.jobsSectionNoBg');
+	var projectMenagerChild = $('.jobsSectionNoBg').find('button.tabletHoverProjectMenager');
+
+	if( displaySize > 320){
+		$('.jobsSectionWithBg').hover(function(){
+			$(this).find('.posloviHover').fadeToggle('slow');
+		});
+		projectMenagerParent.hover(function(){
+			projectMenagerChild.fadeToggle('slow');
+		})
+	} else {
+		return false;
+	};
+	console.log(projectMenager)
+})
+
+
+
